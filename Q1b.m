c8d8824ac4f7308ccb480b416d4e7433324b2868
vishal <subhavishal@yahoo.co.in>
2021-02-16 08:40:43 +0530

    Successful implementation of bocd!

--- Q1b.m
+++ Q1b.m
@@ -5,7 +5,7 @@ load NMRlogWell.mat;
 plot(y);
 N = length(y);
 %% Initialise values
-mu = 1.15;k=0.01;lambda=250;
+mu0 = 1.15;k0=0.01;lambda=250;
 % N+1*N+1 matrix. Value at (r,c) is the probability value that run length
 % is c-1
 P_joint = zeros(N+1);
@@ -15,6 +15,8 @@ H = 1/lambda;
 alpha = zeros(N+1);beta = zeros(N+1);
 alpha(:,1) = 20; beta(:,1) = 2;
 predictive = zeros(N+1,1);
+mu = zeros(N+1); mu(1) = mu0;
+k = zeros(N+1); k(1) = k0;
 % Since MATLAB indexing starts from 1, kth row/column denotes k-1th data
 % point/k-1 run length
 %% Implement bocd
@@ -23,15 +25,14 @@ for t = 1:N
     predictive = zeros(t,1);
     % Evaluate predictive probabilities at different run lengths
     for rt = 1:t % Run length can be 0 till t
-        std_dev = sqrt(beta(t,rt)*(k+1)/(alpha(t,rt)*k));
-        xt_normalised = (xt-mu)/std_dev;
-        predictive(rt) = tpdf(xt_normalised,2*alpha(t,rt)); 
+        std_dev = sqrt(beta(t,rt)*(k(rt)+1)/(alpha(t,rt)*k(rt)));
+        xt_normalised = (xt-mu(rt))./std_dev;
+        predictive(rt) = tpdf(xt_normalised,2*alpha(t,rt))/std_dev; 
     end
     if t ~= 1
         % Growth probabilities
-        for rt = 2:t+1 % Run length can be 1 till t
-            P_joint(t,rt) = (P_joint(t-1,rt-1).*predictive(rt-1))*(1-H);
-        end
+        % Run length can be 1 till t
+        P_joint(t,2:t+1) = (P_joint(t-1,1:t).*predictive(1:t)')*(1-H);
         % Changepoint probability
         P_joint(t,1) = sum(P_joint(t-1,1:t).*predictive(1:t)')*H;
     else
@@ -41,14 +42,16 @@ for t = 1:N
     end
     % Evidence
     P_evidence = sum(P_joint(t,:));
+    P_joint(t,:) = P_joint(t,:)/P_evidence;
     P_runlength(t,:) = P_joint(t,:)/P_evidence;
     % Update statements
-    alpha(t+1,2:N+1) = alpha(t,1:N) + 0.5;
-    beta(t+1,2:N+1) = beta(t,1:N) + k*(xt-mu)^2/(2*(k+1));
-    mu = (k*mu+xt)/(k+1);
-    k = k + 1;
+    alpha(t+1,2:t+1) = alpha(t,1:t) + 0.5;
+    beta(t+1,2:t+1) = beta(t,1:t) + k(1:t).*(xt-mu(1:t)).^2./(2*(k(1:t)+1));
+    mu(2:t+1) = (k(1:t).*mu(1:t)+xt)./(k(1:t)+1);
+    k(2:t+1) = k(1:t) + 1;
     %P_runlength(t,:) = P_runlength(t,:)/sum(P_runlength(t,:));
     if isnan(sum(P_runlength(t,:)))
         break
     end
 end
+plot_rt_probs(P_runlength)
