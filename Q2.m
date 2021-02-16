fbf5fc3b9f7d4a52bd1f76a2f752450a0d5eeaab
vishal <subhavishal@yahoo.co.in>
2021-02-16 16:13:50 +0530

    q2 completed!

--- Q2.m
+++ Q2.m
@@ -120,4 +120,14 @@ cp  = 285;
 %% Part b)
 % From RLS and cp, we have
 theta_initial = theta(285);
-variance_initial = var(y(2:cp)-theta*y(1:cp-1));
+variance_initial = var(y(2:cp)-theta_initial*y(1:cp-1));
+% Post cp
+data = y(286:N);
+l =  length(data);
+parcorr(data,'NumLags',20);
+% Again AR(1) model is sufficient to explain. This is as expected because
+% only the white noise term's variance changes.
+model2 = fitlm(data(1:l-1),data(2:l),'y~x1-1');
+res2 = model2.Residuals.Raw;
+theta_new = model2.Coefficients.Estimate;
+variance_later= var(res2);
